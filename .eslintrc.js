725f498873c19ba33255d7c913403f9dac05f871
Zhang <my@qq.com>
2019-02-18 16:59:33 +0800

    基本配置

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  //使用第三方的规则
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  /* 配置自己的规则 */
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
